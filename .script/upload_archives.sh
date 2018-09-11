e1e0da2377bee11976ed6163fe3aec7ac6db8da4
liuzheng <r_miyagi@qq.com>
2018-09-11 11:26:25 +0800

    ZTONE_NETWORK

---
+++ .script/upload_archives.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+_PROJECT_PATH='/workon/m2/mvn-repo'
+
+git status
+git add ./
+git commit -m "ZTONE_NETWORK"
+git push origin master
+
+if (( $# == 0 )) || [[ -z $1 ]]; then
+    ./gradlew -q -p ztone.lang clean build uploadArchives
+
+else
+    _MODULE_NAME=$1
+
+    ./gradlew -q -p ${_MODULE_NAME} clean build uploadArchives
+fi
+
+git -C ${_PROJECT_PATH} status
+git -C ${_PROJECT_PATH} add ./
+git -C ${_PROJECT_PATH} commit -m "ZTONE_NETWORK"
+git -C ${_PROJECT_PATH} push github master
